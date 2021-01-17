1c911f9682419e075714f056699dd8e9a10a5023
Neelesh Pandey <pandeyneelkumar@gmail.com>
2021-01-17 15:49:21 +0530

    first commit

---
+++ src/main/java/com/upgrad/blog/comupgradblog/ComUpgradBlogApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.blog.comupgradblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComUpgradBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComUpgradBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upgrad/blog/comupgradblog/ComUpgradBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.blog.comupgradblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComUpgradBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
